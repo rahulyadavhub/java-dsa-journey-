26cbc8c978a5be8982dd03f397429f5654b60f27
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-03-08 04:57:54 +0530

    Initial Java DSA Journey

---
+++ MaxArray.java
@@ -0,0 +1,12 @@
+public class MaxArray {
+    public static void main(String[] args) {
+        int[] arr = {5,3,8,2,9};
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest = " + max);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Java DSA Journey
+
+Daily practice of Data Stracture and Algorithms in Java.
+
+Day 1
+- Pattern printing
+- Basic java structure 
