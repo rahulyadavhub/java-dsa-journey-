4959f05b619d620e905696ae4af41a2dda2e852b
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-03-12 00:01:05 +0530

    Update DSA practice

--- arrays/RemoveDuplicates.java
+++ arrays/RemoveDuplicates.java
@@ -1,5 +1,23 @@
 package arrays;
 
 public class RemoveDuplicates {
-    
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,2,2,3,4,4,5};
+
+        int index = 0;
+
+        for(int i = 1; i < arr.length; i++){
+
+            if(arr[i] != arr[index]){
+                index++;
+                arr[index] = arr[i];
+            }
+        }
+
+        for(int i = 0; i <= index; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
